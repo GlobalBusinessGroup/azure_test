97c6c4876fbf14c5caaac29730f2f02ede82e5a6
unknown <n.braspenning@remoticom.com>
2019-09-18 13:55:23 +0200

    aanpasinkuje

--- deploy_test/index.js
+++ deploy_test/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "hello " + (req.query.name || req.body.name)
+            body: "test " + (req.query.name || req.body.name)
         };
     }
     else {
